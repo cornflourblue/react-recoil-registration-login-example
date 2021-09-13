4df958291078e00a8fdd1dd720b3e874fcc1fe93
Jason Watmore <cornflourblue@hotmail.com>
2021-09-13 12:51:42 +1000

    tweaked import statements

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { RecoilRoot } from 'recoil';
 
-import './index.css';
-import { App } from './App';
+import 'index.css';
+import { App } from 'App';
 
 // setup fake backend
-import { fakeBackend } from './_helpers';
+import { fakeBackend } from '_helpers';
 fakeBackend();
 
 ReactDOM.render(
